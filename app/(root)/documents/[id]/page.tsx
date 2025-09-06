3b449682ba86fe9953b8e38659739bda476c3e5e
HkFromMY <bpvper@gmail.com>
2025-09-06 17:44:20 +0800

    feat: done header skeleton

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 
 const Document = () => {
   return (
-    <div>Document</div>
+    <div>
+      <Header>
+        <div className="flex items-center justify-center gap-2">
+          <p className="document-title">Fake doc title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
   )
 }
 
